f9e56d7dac844ca22934c95600f81a12ea969636
Brent Sallows <dumphear@hotmail.com>
2019-08-22 12:37:28 +1000

    end of day 22/08/19

--- VR_FE_API/VR_FE_API/VR_FE_API/Controllers/MovieController.cs
+++ VR_FE_API/VR_FE_API/VR_FE_API/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
@@ -42,6 +43,9 @@ namespace VR_FE_API.Controllers
             {
                 HttpResponseMessage response = WebClient.ApiClient.PostAsJsonAsync("Movies", movie).Result;
 
+                //we will refer to this in the Index.cshtml of the Movie so alertify can display the message
+                TempData["SuccessMessage"] = "Record added successfully";
+
                 return RedirectToAction("Index");
             }
             catch
@@ -66,6 +70,8 @@ namespace VR_FE_API.Controllers
             try
             {
                 HttpResponseMessage response = WebClient.ApiClient.PutAsJsonAsync($"Movies/{id}", movie).Result;
+
+                TempData["SuccessMessage"] = "Record updated successfully";
 
                 return RedirectToAction("Index");
             }
@@ -93,12 +99,25 @@ namespace VR_FE_API.Controllers
                 // TODO: Add delete logic here
                 HttpResponseMessage response = WebClient.ApiClient.DeleteAsync($"Movies/{id}").Result;
 
+                TempData["SuccessMessage"] = "Record deleted successfully";
+
                 return RedirectToAction("Index");
             }
             catch
             {
                 return View();
             }
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach(var file in files)
+            {
+                file.SaveAs(Path.Combine(Server.MapPath("~/UploadedFiles"), file.FileName));
+            }
+
+            return Json("File uploaded Successfully.");
         }
     }
 }
